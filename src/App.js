9bed1754f1a0300e3c5e3386cb030b9b14b493c7
“brunobatista” <brunobatista101@gmail.com>
2021-02-02 09:42:43 -0300

    tirando gabartito

--- src/App.js
+++ src/App.js
@@ -1,16 +1,15 @@
-import React from 'react';
+import React from 'react';	import React from 'react';
 import Header from './components/Header';
 import MovieList from './components/MovieList';
-import './App.css';
+import './App.css';	import './App.css';
 import movies from './data';
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <MovieList movies={ movies } />
-    </div>
-  );
-}
 
-export default App;
+function App() {	
+  return (	 
+    <div className="App">	    
+      {/* Sua implementação deve ficar aqui. Remova essas duas linhas e mão na massa */}	      
+      Hora de codar! 🚀✍️👨‍💻👩‍💻💪	      
+    </div>	  
+  );	  
+}
